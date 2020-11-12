2a16dafd2aa2204699c60ae0ecd2bc14b94bede9
cj-rupp <72820354+cj-rupp@users.noreply.github.com>
2020-11-12 19:41:19 +0000

    Entry Page funcionality

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
+from html import unescape
 
 from . import util
+from markdown2 import Markdown
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def post(request, title):
+
+    converter = Markdown()
+
+    marked = util.get_entry(title)
+    text = converter.convert(marked)
+
+    if marked != None:
+        return render(request, "encyclopedia/page.html", {
+            "title": title,
+            "text": text
+        })
 
