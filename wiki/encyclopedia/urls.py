f44233729b05049dd269b50f325edcdc7b1e7120
cj-rupp <72820354+cj-rupp@users.noreply.github.com>
2020-11-14 12:19:58 +0000

    Search functions

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -6,5 +6,6 @@ app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
     path("random", views.random, name="random"),
+    path("search", views.search, name="search"),
     path("wiki/<str:title>",views.post, name="post")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from html import unescape
+from django import forms
+import re
 
 from . import util
 from markdown2 import Markdown
 
 from random import choice
+
+class SearchForm(forms.Form):
+    q = forms.CharField(label="search")
 
 converter = Markdown()
 
@@ -35,5 +39,42 @@ def random(request):
         "title": selected,
         "text": converter.convert(marked)
     })
+
+def search(request):
+    if request.method == "POST":
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            pattern = form.cleaned_data["q"]
+            text = util.get_entry(pattern)
+            if text != None:
+                return render(request, "encyclopedia/page.html", {
+                    "title": pattern,
+                    "text": converter.convert(text) 
+                })
+            else:
+                matcher = re.compile(pattern)
+                entries = util.list_entries()
+                found = []
+                for entry in entries:
+                    if(matcher.search(entry)) :
+                        found.append(entry)
+                if len(found) > 0:
+                    return render(request, "encyclopedia/result.html", {
+                        "matches": found
+                    })
+                else:
+                    return render(request, "encyclopedia/error.html", {
+                        "title": pattern
+                    })
+        else:
+            return render(request, "encyclopedia/error.html", {
+                "title": form.errors
+            })
+    else:
+        return render(request, "encyclopedia/error.html", {
+            "title": "method"        
+        })
+
+
 
 
